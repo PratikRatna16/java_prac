ddec59ad3ffb14fca3db23d33b51fa56ab57993c
PratikRatna16 <beherapratik006@gmail.com>
2025-09-16 10:08:40 +0530

    Initial commit

---
+++ com/company/class2.java
@@ -0,0 +1,10 @@
+package com.company;
+class Animal {
+    int legs;
+        }
+        class birds{
+    int legs;
+        }
+        class reptiles{
+    int eyes;
+        }
---
+++ com/company/class3.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class class3 {
+    public static void main(String[] args){
+        Animal pratik = new Animal();
+        pratik.legs=7;
+        int legs = pratik.legs;
+
+    }
+}
